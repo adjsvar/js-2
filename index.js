1b6a02a61b28ae4a0b88a395e502db9f66eea55d
Adrian Silva <adjsvar@gmail.com>
2022-10-01 10:52:27 -0300

    openbootcamp 2

---
+++ index.js
@@ -0,0 +1,10 @@
+const cumpleanyo = new Date(1989, 07, 20);
+const hpAnyo = new Date(1997, 05, 26);
+
+const libro = {
+    titulo: "Harry Potter",
+    autor: "J.K Rowling",
+    fecha: hpAnyo,
+}
+
+const adri = ["Adrian", 33, true, cumpleanyo, libro];
